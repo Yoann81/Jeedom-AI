4a192ee246af85badcda85cd0506ca93aa062216
Yoann81 <raven18195@gmail.com>
2025-12-28 23:17:06 +0100

    Correction affichage equipement

--- desktop/php/ai_connector.php
+++ desktop/php/ai_connector.php
@@ -20,46 +20,6 @@ $eqLogics = eqLogic::byType('ai_connector');
             <span>{{Configuration}}</span>
         </div>
 
-        <legend><i class="fas fa-terminal"></i> {{Démon}}</legend>
-        <div class="form-horizontal" style="padding: 10px;">
-            <div class="form-group">
-                <label class="col-sm-2 control-label">{{État du démon}}</label>
-                <div class="col-sm-2">
-                    <center>
-                        <span id="sun_deamon_state"></span>
-                    </center>
-                </div>
-                <label class="col-sm-2 control-label">{{Gestion}}</label>
-                <div class="col-sm-4">
-                    <a class="btn btn-success btn-sm deamonLaunch"><i class="fas fa-play"></i> {{Lancer}}</a>
-                    <a class="btn btn-danger btn-sm deamonStop"><i class="fas fa-stop"></i> {{Arrêter}}</a>
-                </div>
-            </div>
-        </div>
-
-        <legend><i class="fas fa-microphone"></i> {{Configuration Vocale Globale}}</legend>
-        <div class="form-horizontal" style="padding: 10px;">
-            <div class="form-group">
-                <label class="col-sm-2 control-label">{{Activer la voix}}</label>
-                <div class="col-sm-1">
-                    <input type="checkbox" class="configKey" data-l1key="voice_enable" />
-                </div>
-                <label class="col-sm-2 control-label">{{Commande destination}}</label>
-                <div class="col-sm-3">
-                    <div class="input-group">
-                        <input type="text" class="configKey form-control" data-l1key="voice_cmd_id" />
-                        <span class="input-group-btn">
-                            <a class="btn btn-default bt_selectCmdExpression"><i class="fas fa-list-alt"></i></a>
-                        </span>
-                    </div>
-                </div>
-                <label class="col-sm-1 control-label">{{Micro}}</label>
-                <div class="col-sm-1">
-                    <input type="text" class="configKey form-control" data-l1key="voice_device_id" placeholder="1"/>
-                </div>
-            </div>
-        </div>
-
         <legend><i class="fas fa-robot"></i> {{Mes IA}}</legend>
         <div class="eqLogicThumbnailContainer">
             <?php
@@ -83,11 +43,13 @@ $eqLogics = eqLogic::byType('ai_connector');
                 <a class="btn btn-danger btn-sm eqLogicAction" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
             </span>
         </div>
+        
         <ul class="nav nav-tabs" role="tablist">
             <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
             <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
             <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-list-alt"></i> {{Commandes}}</a></li>
         </ul>
+
         <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;display:overflow:visible">
             <div role="tabpanel" class="tab-pane active" id="eqlogictab">
                 <br/>
@@ -120,6 +82,33 @@ $eqLogics = eqLogic::byType('ai_connector');
                                 <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
                             </div>
                         </div>
+
+                        <br>
+                        <legend><i class="fas fa-microphone"></i> {{Configuration Vocale}}</legend>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{Activer l'écoute}}</label>
+                            <div class="col-sm-3">
+                                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="voice_enable" />{{Activer}}</label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{Commande de retour (HP)}}</label>
+                            <div class="col-sm-4">
+                                <div class="input-group">
+                                    <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="voice_cmd_id" />
+                                    <span class="input-group-btn">
+                                        <a class="btn btn-default bt_selectCmdExpression"><i class="fas fa-list-alt"></i></a>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{ID Micro (Index)}}</label>
+                            <div class="col-sm-2">
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="voice_device_id" placeholder="1"/>
+                            </div>
+                        </div>
+
                         <br>
                         <legend><i class="fas fa-key"></i> {{Paramètres de l'IA}}</legend>
                         <div class="form-group">
@@ -134,7 +123,7 @@ $eqLogics = eqLogic::byType('ai_connector');
                         </div>
                         <div class="form-group">
                             <label class="col-sm-3 control-label">{{Clé API}}</label>
-                            <div class="col-sm-3">
+                            <div class="col-sm-5">
                                 <input type="password" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="apiKey" placeholder="{{Votre clé secrète}}"/>
                             </div>
                         </div>
@@ -147,16 +136,13 @@ $eqLogics = eqLogic::byType('ai_connector');
                     </fieldset>
                 </form>
             </div>
+            
             <div role="tabpanel" class="tab-pane" id="commandtab">
                 <br/>
                 <table id="table_cmd" class="table table-bordered table-condensed">
                     <thead>
                         <tr>
-                            <th style="width: 50px;">#</th>
-                            <th style="width: 250px;">{{Nom}}</th>
-                            <th style="width: 150px;">{{Type}}</th>
-                            <th style="width: 150px;">{{Options}}</th>
-                            <th style="width: 100px;">{{Action}}</th>
+                            <th>{{Nom}}</th><th>{{Options}}</th><th>{{Action}}</th>
                         </tr>
                     </thead>
                     <tbody>
