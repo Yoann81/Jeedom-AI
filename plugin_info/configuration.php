f1dd6085d87be95b1b4a59203dfbc5ec0d70d8ee
Yoann81 <raven18195@gmail.com>
2025-12-29 00:05:07 +0100

    Modification interface

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -1,12 +1,4 @@
 <?php
-/* This file is part of Jeedom.
-*
-* Jeedom is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*/
-
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 include_file('core', 'authentification', 'php');
 if (!isConnect()) {
@@ -14,3 +6,8 @@ if (!isConnect()) {
     die();
 }
 ?>
+
+<form class="form-horizontal">
+    <fieldset>
+        </fieldset>
+</form>
