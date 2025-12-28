2da64627ba431ae694684f17749807462bd96cd5
Yoann81 <raven18195@gmail.com>
2025-12-28 21:53:52 +0100

    Modification daemon

--- core/class/ai_connector.class.php
+++ core/class/ai_connector.class.php
@@ -77,6 +77,25 @@ class ai_connector extends eqLogic {
     public static function deamon_stop() {
         exec("ps aux | grep ai_connector_daemon.py | grep -v grep | awk '{print $2}' | xargs kill -9 > /dev/null 2>&1");
     }
+    public static function deamon_info() {
+        $return = array();
+        $return['log'] = 'ai_connector_daemon';
+        $return['launchable'] = 'ok';
+        
+        // On vérifie si le processus python tourne
+        $state = exec("ps aux | grep ai_connector_daemon.py | grep -v grep");
+        if ($state != "") {
+            $return['state'] = 'ok';
+        } else {
+            $return['state'] = 'nok';
+        }
+        return $return;
+    }
+
+    public static function deamon_stop() {
+        // Tue le démon proprement
+        exec("ps aux | grep ai_connector_daemon.py | grep -v grep | awk '{print $2}' | xargs kill -9 > /dev/null 2>&1");
+    }
 
     /**
      * EXÉCUTION DES COMMANDES
