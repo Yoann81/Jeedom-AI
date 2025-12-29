f0ce1974b45d59bffdd8d398d81fb7e1710c37b3
Yoann81 <raven18195@gmail.com>
2025-12-29 10:23:44 +0100

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -17,14 +17,23 @@
 
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
-// Fonction exécutée automatiquement après l'installation du plugin
-function template_install() {
+// Fonction exécutée automatiquement après l'installation ou l'activation du plugin
+function ai_connector_install() {
+    require_once dirname(__FILE__) . '/../core/class/ai_connector.class.php';
+    log::add('ai_connector', 'info', 'Fonction _install() appelée. Tentative de démarrage du démon.');
+    ai_connector::deamon_start();
 }
 
 // Fonction exécutée automatiquement après la mise à jour du plugin
-function template_update() {
+function ai_connector_update() {
+    require_once dirname(__FILE__) . '/../core/class/ai_connector.class.php';
+    log::add('ai_connector', 'info', 'Fonction _update() appelée. Tentative de redémarrage du démon.');
+    ai_connector::deamon_start();
 }
 
 // Fonction exécutée automatiquement après la suppression du plugin
-function template_remove() {
+function ai_connector_remove() {
+    require_once dirname(__FILE__) . '/../core/class/ai_connector.class.php';
+    log::add('ai_connector', 'info', 'Fonction _remove() appelée. Tentative d\'arrêt du démon.');
+    ai_connector::deamon_stop();
 }
