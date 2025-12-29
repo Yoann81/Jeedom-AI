c31e7fe33545055f989251251191adef0aeaad42
Yoann81 <raven18195@gmail.com>
2025-12-30 00:15:27 +0100

    Update ai_connector_daemon.py

--- resources/demond/ai_connector_daemon.py
+++ resources/demond/ai_connector_daemon.py
@@ -17,6 +17,15 @@ except ImportError:
     PORCUPINE_AVAILABLE = False
     print("Avertissement : Picovoice Porcupine n est pas installe.")
 
+from ctypes import *
+
+# Fonction pour supprimer les messages d'erreur ALSA dans le terminal
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+def py_error_handler(filename, line, function, err, fmt):
+    pass
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+asound = cdll.LoadLibrary('libasound.so.2')
+asound.snd_lib_error_set_handler(c_error_handler)
 
 # --- Configuration par défaut ---
 JEEDOM_URL = "http://127.0.0.1/core/api/jeeApi.php"
