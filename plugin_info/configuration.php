e56e760bdfc18d16d5e98afb1df76915bcbaa4aa
Yoann81 <raven18195@gmail.com>
2025-12-29 01:14:18 +0100

    suppression bloc demon

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -9,12 +9,5 @@ if (!isConnect()) {
 
 <form class="form-horizontal">
     <fieldset>
-        <div class="form-group">
-            <label class="col-lg-4 control-label">{{Gestion du démon}}</label>
-            <div class="col-lg-8">
-                <span class="label" id="deamon_status" style="font-size:1em;"></span>
-                <a class="btn btn-success deamon_start" style="position:relative;top:2px;"><i class="fa fa-play"></i> {{Lancer/Relancer}}</a>
-            </div>
-        </div>
-    </fieldset>
+        </fieldset>
 </form>
