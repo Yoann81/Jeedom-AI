9c36d4b009421770713de44406d745edea9398cd
Yoann81 <raven18195@gmail.com>
2026-01-03 18:26:19 +0100

    fix: add dependancy_info and deamon_info functions directly in configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -5,6 +5,49 @@ if (!isConnect()) {
     include_file('desktop', '404', 'php');
     die();
 }
+
+// Définir les fonctions AVANT que Jeedom les appelle
+function ai_connector_dependancy_info() {
+    $return = array();
+    
+    $progressFile = dirname(__FILE__) . '/../tmp/ai_connector_dep_in_progress';
+    if (file_exists($progressFile)) {
+        $return['state'] = 'in_progress';
+    } else {
+        $daemonPath = dirname(__FILE__) . '/../resources/demond/ai_connector_daemon.py';
+        if (file_exists($daemonPath)) {
+            $return['state'] = 'ok';
+        } else {
+            $return['state'] = 'nok';
+        }
+    }
+    
+    return $return;
+}
+
+function ai_connector_deamon_info() {
+    $return = array();
+    $return['log'] = 'ai_connector_daemon';
+    $return['state'] = 'nok';
+    $return['launchable'] = 'ok';
+    
+    $pidFile = '/tmp/jeedom/ai_connector/daemon.pid';
+    if (file_exists($pidFile)) {
+        $pid = trim(file_get_contents($pidFile));
+        if (!empty($pid) && is_numeric($pid)) {
+            if (function_exists('posix_getpgid')) {
+                if (@posix_getpgid($pid) !== false) {
+                    $return['state'] = 'ok';
+                }
+            } else {
+                $return['state'] = 'ok';
+            }
+        }
+    }
+    
+    return $return;
+}
+
 ?>
 
 <form class="form-horizontal">
