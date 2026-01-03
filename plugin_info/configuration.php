16b37200ed45090e5874c7557dcb21809e408dc0
Yoann81 <raven18195@gmail.com>
2026-01-03 18:12:53 +0100

    feat: restore dependencies and daemons status display in general configuration

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -31,5 +31,78 @@ if (!isConnect()) {
             </div>
         </div>
 
+        <hr>
+        <legend><i class="fas fa-cogs"></i> État du système</legend>
+        
+        <div class="form-group">
+            <label class="col-lg-4 control-label">Dépendances</label>
+            <div class="col-lg-8">
+                <div id="div_dependancy">
+                    <span class="label label-warning">
+                        <i class="fas fa-refresh"></i> Vérification en cours...
+                    </span>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-lg-4 control-label">Démons</label>
+            <div class="col-lg-8">
+                <div id="div_daemon">
+                    <span class="label label-warning">
+                        <i class="fas fa-refresh"></i> Vérification en cours...
+                    </span>
+                </div>
+            </div>
+        </div>
+
     </fieldset>
 </form>
+
+<script>
+$(document).ready(function() {
+    // Vérifier les dépendances via AJAX
+    $.ajax({
+        url: '/plugins/ai_connector/core/ajax/ai_connector.ajax.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            action: 'dependancy_info'
+        },
+        success: function(data) {
+            if (data.state === 'ok') {
+                $('#div_dependancy').html('<span class="label label-success"><i class="fas fa-check"></i> OK</span>');
+            } else if (data.state === 'in_progress') {
+                $('#div_dependancy').html('<span class="label label-info"><i class="fas fa-cog fa-spin"></i> Installation en cours...</span>');
+            } else {
+                $('#div_dependancy').html('<span class="label label-danger"><i class="fas fa-times"></i> Erreur</span>');
+            }
+        },
+        error: function() {
+            $('#div_dependancy').html('<span class="label label-danger"><i class="fas fa-times"></i> Erreur de vérification</span>');
+        }
+    });
+
+    // Vérifier les démons via AJAX
+    $.ajax({
+        url: '/plugins/ai_connector/core/ajax/ai_connector.ajax.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            action: 'deamon_info'
+        },
+        success: function(data) {
+            if (data.state === 'ok') {
+                $('#div_daemon').html('<span class="label label-success"><i class="fas fa-check"></i> Démon actif</span>');
+            } else if (data.state === 'nok') {
+                $('#div_daemon').html('<span class="label label-danger"><i class="fas fa-times"></i> Démon arrêté</span>');
+            } else {
+                $('#div_daemon').html('<span class="label label-warning"><i class="fas fa-question"></i> Statut inconnu</span>');
+            }
+        },
+        error: function() {
+            $('#div_daemon').html('<span class="label label-warning"><i class="fas fa-question"></i> Vérification impossible</span>');
+        }
+    });
+});
+</script>
